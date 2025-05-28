f6e5dd6f81639ddb12e87048c7897f30a5b0515d
Phakamani_Sidimba <psidimba@gmail.com>
2025-05-29 01:15:22 +0200

    Correcting erros on financial_calculator.py

--- finance_calculator.py
+++ finance_calculator.py
@@ -6,4 +6,3 @@ projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are ${int(monthly_savings)}.")
 print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
-
